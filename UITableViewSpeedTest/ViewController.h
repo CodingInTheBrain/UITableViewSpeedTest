d9e73f0ecdb5daff5d5943e9c0790a6626627ee6
surio <surio@Tenda112233.local>
2018-06-13 13:58:20 +0800

    Initial Commit

---
+++ UITableViewSpeedTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewSpeedTest
+//
+//  Created by surio on 2018/6/13.
+//  Copyright © 2018年 surio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewSpeedTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITableViewSpeedTest
+//
+//  Created by surio on 2018/6/13.
+//  Copyright © 2018年 surio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
